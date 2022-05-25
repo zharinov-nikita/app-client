5d6bd109d90ffb805e8f45792e08deabb0e56256
Nikita <zharinov.nicita@gmail.com>
2022-05-25 15:27:52 +0700

    1.0.3

--- src/microservices/link/components/LinkItem/LinkItemButtons.js
+++ src/microservices/link/components/LinkItem/LinkItemButtons.js
@@ -7,19 +7,28 @@ import { useDispatch } from 'react-redux'
 import ApiLink from '../../api'
 
 const LinkItemButtons = ({ document }) => {
-    const { _id } = document
+
+    const { _id, offer, model, title, description, url, short } = document
+    const link = { _id, offer, model, title, description, url, short }
     const dispatch = useDispatch()
+
     return (
         <div>
             <Button
                 size='small' children={<ProjectOutlined />}
                 onClick={() => {
-                    dispatch({ type: 'LINK_UPDATE_FORM', ...document, action: 'update', isShort: document.short })
+                    dispatch({
+                        type: 'LINK_UPDATE_FORM',
+                        ...link,
+                        action: 'update',
+                        isShort: short
+                    })
                 }}
 
             />
             <Button
-                size='small' children={<DeleteOutlined />}
+                size='small'
+                children={<DeleteOutlined />}
                 onClick={() => dispatch(ApiLink.delete(_id))}
             />
         </div>
