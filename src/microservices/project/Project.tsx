f2f725bc9111d171143ccd25e7408b8d73632433
Nikita <zharinov.nicita@gmail.com>
2022-06-07 16:46:57 +0700

    Изменение статуса проекта

--- src/microservices/project/Project.tsx
+++ src/microservices/project/Project.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux'
 import { projectSlice } from './store/project'
 
+import { Select } from 'antd';
+
+
+const { Option } = Select;
+
+
+
 const Project: React.FC = () => {
     const dispatch = useAppDispatch()
-    const { completedTask } = projectSlice.actions
+    const { completedTask, statusProject } = projectSlice.actions
     const projects = useAppSelector(state => state.project.projects)
+
     return (
         <>
             {projects.map(({ id, name, status, tasks }) =>
                 <div key={id}>
                     <h4>{name}</h4>
-                    <h4>{status}</h4>
+                    <Select defaultValue={status} style={{ width: 120 }} onChange={(value: string) => dispatch(statusProject({ id, name, tasks, status: value }))}>
+                        <Option value="запланировано">запланировано</Option>
+                        <Option value="работа">работа</Option>
+                        <Option value="завершено">завершено</Option>
+                    </Select>
                     {tasks.map(({ projectId, id, name, date, completed }) =>
                         <ul key={id}>
                             <li>
-                                <p>{name}</p>
+                                <span>{name}</span>
                                 <mark>{date}</mark>
                                 <input
                                     type="checkbox"
@@ -25,6 +37,7 @@ const Project: React.FC = () => {
                             </li>
                         </ul>
                     )}
+                    <hr />
                 </div>
             )}
         </>
--- src/microservices/project/store/project.ts
+++ src/microservices/project/store/project.ts
@@ -28,10 +28,8 @@ const initialState: InitialStateType = {
             name: 'сео',
             status: 'запланировано',
             tasks: [
-                { projectId: 0, id: '0 - сео', name: 'Главный ключ', date: '24.08.2000', completed: false },
-                { projectId: 0, id: '1 - сео', name: 'Дополнительный ключ - 1', date: '24.08.2000', completed: false },
-                { projectId: 0, id: '2 - сео', name: 'Дополнительный ключ - 2', date: '24.08.2000', completed: false },
-                { projectId: 0, id: '3 - сео', name: 'Дополнительный ключ - 3', date: '24.08.2000', completed: false },
+                { projectId: 0, id: 0, name: 'Главный ключ', date: '24.08.2000', completed: false },
+                { projectId: 0, id: 1, name: 'Дополнительный ключ - 1', date: '24.08.2000', completed: false },
             ]
         },
         {
@@ -39,10 +37,17 @@ const initialState: InitialStateType = {
             name: 'контент',
             status: 'запланировано',
             tasks: [
-                { projectId: 1, id: '0 - контент', name: 'Главный ключ', date: '24.08.2000', completed: false },
-                { projectId: 1, id: '1 - контент', name: 'Дополнительный ключ - 1', date: '24.08.2000', completed: false },
-                { projectId: 1, id: '2 - контент', name: 'Дополнительный ключ - 2', date: '24.08.2000', completed: false },
-                { projectId: 1, id: '3 - контент', name: 'Дополнительный ключ - 3', date: '24.08.2000', completed: false },
+                { projectId: 1, id: 0, name: 'Статья #1', date: '24.08.2000', completed: false },
+                { projectId: 1, id: 1, name: 'Статья #2', date: '24.08.2000', completed: false },
+            ]
+        },
+        {
+            id: 2,
+            name: 'настройки',
+            status: 'запланировано',
+            tasks: [
+                { projectId: 2, id: 0, name: 'Доменное имя', date: '24.08.2000', completed: false },
+                { projectId: 2, id: 1, name: 'Ssl сертификат', date: '24.08.2000', completed: false },
             ]
         }
     ]
@@ -53,8 +58,16 @@ export const projectSlice = createSlice({
     initialState,
     name: 'project',
     reducers: {
-        completedTask(state: InitialStateType, action: { payload: TaskType }) {
+        statusProject(state: InitialStateType, action: { payload: ProjectType }) {
+            state.projects = state.projects.map(project => {
+                if (project.id === action.payload.id) {
+                    return { ...project, status: action.payload.status }
+                }
+                return project
+            })
+        },
 
+        completedTask(state: InitialStateType, action: { payload: TaskType }) {
             state.projects[action.payload.projectId].tasks = state.projects[action.payload.projectId].tasks.map(task => {
                 if (task.id === action.payload.id) {
                     return { ...task, completed: !task.completed }
