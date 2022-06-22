b2722cb3e1b027607a124e3894fec97f3907bec8
Nikita <zharinov.nicita@gmail.com>
2022-06-22 10:41:10 +0700

    feature/prooject #35

--- src/modules/project/store/types/drawer.type.ts
+++
@@ -1,3 +0,0 @@
-export type DrawerType = {
-    visible: boolean
-}
--- src/modules/project/store/types/state.type.ts
+++
@@ -1,9 +0,0 @@
-import { DrawerType } from "./drawer.type"
-import { FormType } from "./form.type"
-import { OfferType } from "./offer.type"
-
-export type StateType = {
-    offers: OfferType[]
-    drawer: DrawerType
-    form: FormType
-}
